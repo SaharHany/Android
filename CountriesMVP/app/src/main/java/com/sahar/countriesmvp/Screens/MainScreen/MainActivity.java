f725bc49e029623973c87082f6f242095e9f0f37
Sahar Hany <sahar96hany@gmail.com>
2019-04-01 20:09:41 +0200

    create new App CountriesMVP

---
+++ CountriesMVP/app/src/main/java/com/sahar/countriesmvp/Screens/MainScreen/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sahar.countriesmvp.Screens.MainScreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sahar.countriesmvp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
